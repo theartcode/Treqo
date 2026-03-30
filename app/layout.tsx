f10ce5aac54032dbf56021cd4f2f61396be34b02
theartcode <theartcode01@gmail.com>
2026-03-30 18:50:39 +0530

    google analytrics

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,22 +21,38 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Meta Pixel Script */}
-        <Script id="facebook-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1511250130615837');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-      </head>
+  {/* Facebook Pixel */}
+  <Script id="facebook-pixel" strategy="afterInteractive">
+    {`
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1511250130615837');
+      fbq('track', 'PageView');
+    `}
+  </Script>
+
+  {/* ✅ Google Analytics Script (LOAD) */}
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-01S7JF3G13"
+    strategy="afterInteractive"
+  />
+
+  {/* ✅ Google Analytics Config */}
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-01S7JF3G13');
+    `}
+  </Script>
+</head>
 
       <body className={inter.className}>
         {children}
